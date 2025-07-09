b4ee4803711d4fef6c43c969b820dfd88638ffb5
Nicolas Goncalves De Santana <nde-sant@c1r2p4.42sp.org.br>
2025-07-09 15:25:26 -0300

    add ex12

---
+++ ex12/ft_iterative_factorial.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/09 15:18:39 by nde-sant          #+#    #+#             */
+/*   Updated: 2025/07/09 15:25:04 by nde-sant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_iterative_factorial(int nb)
+{
+	int	result;
+
+	result = 1;
+	if (nb <= 0)
+		return (0);
+	while (nb > 0)
+		result *= nb--;
+	return (result);
+}
