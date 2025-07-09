38b10eb22c99ae406740d6f5212970066caaa537
Nicolas Goncalves De Santana <nde-sant@c1r2p4.42sp.org.br>
2025-07-09 15:44:14 -0300

    add ex13

---
+++ ex13/ft_recursive_factorial.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/09 15:28:12 by nde-sant          #+#    #+#             */
+/*   Updated: 2025/07/09 15:42:48 by nde-sant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb <= 0)
+		return (0);
+	else if (nb > 1)
+		return (nb * ft_recursive_factorial(nb - 1));
+	else
+		return (1);
+}
